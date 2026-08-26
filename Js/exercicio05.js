e7730a3d75331e40065f66d3a7627acfc755e0d6
Raphael Santos <raphaell.guilherme@hotmail.com>
2026-08-25 21:56:01 -0300

    feat: classifica situacao eleitoral por idade

---
+++ Js/exercicio05.js
@@ -0,0 +1,23 @@
+let idade = prompt("Qual a sua idade ?");
+
+//convertendo string para numero para realizar a regra 
+ let idadeDigitada = Number (idade);
+ const apenasDigitos = /^\d+$/;  //variavel para aceitar somente numeros
+
+
+
+//declarando a regra para voto
+if ( !apenasDigitos.test(idade) || idadeDigitada ==="" || idadeDigitada ==="null" || idadeDigitada <= 0 || isNaN (idade)){
+    alert ("Idade invalida !!")
+
+}else if (idade < 16){
+    console.log (`Pela sua idade que é de: ${idade} anos, não é permitido votar`) 
+
+}else if (idade >= 16 && idade <=17, idade >=70) {
+    console.log (`Pela sua idade que é de:  ${idade} anos, seu voto é opcional`)
+
+}else {
+    console.log(`Pela sua idade que é de:  ${idade} anos, seu voto é obrigatório`);
+
+}
+
