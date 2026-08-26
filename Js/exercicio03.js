a7baa1ba0768bff4332e20e4b2184ffae5406b54
Raphael Santos <raphaell.guilherme@hotmail.com>
2026-08-25 21:50:16 -0300

    feat: coleta e apresenta dados do aluno

---
+++ Js/exercicio03.js
@@ -0,0 +1,10 @@
+const nome = prompt ("Digite seu nome");
+let idade = prompt ("Digite sua idade");
+let cidade = prompt ("Digite a sua cidade atual");
+let curso = prompt ("Qual curso está matriculado ?");
+
+
+
+console.log ("Olá meu nome é: " +nome+ " tenho " +idade+ " anos, moro na cidade " +cidade+ " e estou cursando " +curso );
+
+console.log (`Olá meu nome é: ${nome}, tenho ${idade} anos, moro na cidade ${cidade} e estou cursando ${curso} `);
