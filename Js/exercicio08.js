801f361c6c34917c9c20cc6ac62bbfec1d456e08
Raphael Santos <raphaell.guilherme@hotmail.com>
2026-08-25 22:04:51 -0300

    feat: gera tabuada utilizando for

---
+++ Js/exercicio08.js
@@ -0,0 +1,11 @@
+ //declarando as variaveis
+ 
+const numero = prompt ("Digite o numero desejada para gerar tabuada");
+ 
+ 
+//condicional para a tabuada
+for (let i = 1; i <= 10; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+}
+ 
+
