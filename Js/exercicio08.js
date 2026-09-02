6b019d1f8a225231e221755755741ea4f035ed88
Raphael Santos <raphaell.guilherme@hotmail.com>
2026-09-01 22:28:34 -0300

    corrigindo validação

--- Js/exercicio08.js
+++ Js/exercicio08.js
@@ -1,7 +1,18 @@
  //declarando as variaveis
  
-const numero = prompt ("Digite o numero desejada para gerar tabuada");
- 
+let numero = prompt ("Digite o numero desejada para gerar tabuada");
+
+
+// Convertendo o texto digitado para número
+numero = Number(numero);
+
+
+// Verifica se é numérico e se é um número inteiro
+while (isNaN(numero) || !Number.isInteger(numero)) {
+    numero = Number(prompt("Valor inválido! Digite um número inteiro:"));
+}
+
+
  
 //condicional para a tabuada
 for (let i = 1; i <= 10; i++) {
